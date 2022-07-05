29a6053ba7a90359f0d33537d3bf374f3e17896d
bcentner <bcentner@umich.edu>
2022-07-05 14:51:41 -0400

    mistakes needed

--- forYouRequest.py
+++ forYouRequest.py
@@ -6,6 +6,11 @@ import requests
 response = requests.get('https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1')
 
 if response.status_code == 200:
-    print("yay")
+    print("Status code 200")
 else:
     print("oops\n" + response.status_code)
+    exit
+
+jsonResponse = response.json() # fixme
+for key, value in jsonResponse.items():
+    print(key, ":", value)
