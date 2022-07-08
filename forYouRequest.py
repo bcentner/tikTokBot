8705c00bdaabc2d010c140d4f4947e42767a5e97
bcentner <bcentner@umich.edu>
2022-07-08 16:05:44 -0400

    poking around with menu

--- forYouRequest.py
+++ forYouRequest.py
@@ -2,15 +2,16 @@ from urllib import response
 import requests
 
 # https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1
+def runMe():
+	response = requests.get('https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1')
 
-response = requests.get('https://www.tiktok.com/foryou?is_copy_url=1&is_from_webapp=v1')
+	if response.status_code == 200:
+	    print("Status code 200")
+	else:
+	    print("oops\n" + response.status_code)
+	    exit
 
-if response.status_code == 200:
-    print("Status code 200")
-else:
-    print("oops\n" + response.status_code)
-    exit
-
-jsonResponse = response.json() # fixme
-for key, value in jsonResponse.items():
-    print(key, ":", value)
+	print(response.text)
+	jsonResponse = response.json() # fixme
+	for key, value in jsonResponse.items():
+	    print(key, ":", value)
---
+++ main.py
@@ -0,0 +1,16 @@
+import forYouRequest
+# main menu for tiktok bot
+
+print(" _____ _ _  _____     _   ______       _                        \n" +
+"|_   _(_) ||_   _|   | |  | ___ \     | |                       \n" +
+"  | |  _| | _| | ___ | | _| |_/ / ___ | |_ _ __ ___   ___ _ __  \n" +
+"  | | | | |/ / |/ _ \| |/ / ___ \/ _ \| __| '_ ` _ \ / _ \ '_ \ \n" +
+"  | | | |   <| | (_) |   <| |_/ / (_) | |_| | | | | |  __/ | | |\n" +
+"  \_/ |_|_|\_\_/\___/|_|\_\____/ \___/ \__|_| |_| |_|\___|_| |_|\n")
+opt = input("\nSelect your option: \n  1)Scrape main page\n  2)Login\n  3)FIXME...\n\n>")
+if opt == 1:
+    forYouRequest.runMe()
+elif opt == 2:
+    print("IN WORK")
+elif opt == 3:
+	print("you selected three")
